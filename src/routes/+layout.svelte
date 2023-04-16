59b81fd9bc6b97b80ab00e41258ebab7cdcfd589
vdud <varundudeja96@gmail.com>
2023-04-16 09:22:00 +0530

    scrollingUpToDate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,27 +12,60 @@
 		topNav = document.getElementById('TopNav');
 		bottomNav = document.getElementById('BottomNav');
 		slot = document.getElementById('slot');
+
+		bottomNav.style.bottom = '-4.2rem';
 	});
 
 	let lastScrollPosition = 0;
+
 	const handleScroll = () => {
 		const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 		const isScrollingDown = currentScrollPosition > lastScrollPosition;
+		const isScrollingUp = currentScrollPosition < lastScrollPosition;
 
-		const isScrollEnd = currentScrollPosition + window.innerHeight >= slot.offsetHeight;
-		if (isScrollingDown && currentScrollPosition > 100 && !isScrollEnd) {
+		// const isScrollEnd = currentScrollPosition + window.innerHeight >= slot.offsetHeight;
+		const isScrollStart = currentScrollPosition < 100;
+		// const isScrollStart = currentScrollPosition === 0;
+
+		if (isScrollingDown && !isScrollStart && !isScrollingUp) {
 			topNav.style.top = '-6.2rem';
-			bottomNav.style.bottom = '-4.2rem';
-		} else if (isScrollEnd) {
-			topNav.style.top = '-6.2rem';
+			// bottomNav.style.bottom = '-4.2rem';s
 			bottomNav.style.bottom = '0';
-		} else {
+		} else if (isScrollStart) {
 			topNav.style.top = '0';
-			bottomNav.style.bottom = '0';
+			bottomNav.style.bottom = '-4.2rem';
+		} else if (isScrollingUp) {
+			topNav.style.top = '0';
+			bottomNav.style.bottom = '-4.2rem';
 		}
-
 		lastScrollPosition = currentScrollPosition;
 	};
+
+	// const handleScroll = () => {
+	// 	const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+	// 	const isScrollingDown = currentScrollPosition > lastScrollPosition;
+
+	// 	const isScrollEnd = currentScrollPosition + window.innerHeight >= slot.offsetHeight;
+	// 	const isScrollStart = currentScrollPosition === 0;
+
+	// 	// if (isScrollingDown && currentScrollPosition > 100 && !isScrollEnd) {
+	// 	if (isScrollingDown && !isScrollEnd && !isScrollStart) {
+	// 		topNav.style.top = '-6.2rem';
+	// 		// bottomNav.style.bottom = '-4.2rem';
+	// 		bottomNav.style.bottom = '0';
+	// 	} else if (isScrollEnd) {
+	// 		topNav.style.top = '-6.2rem';
+	// 		bottomNav.style.bottom = '0';
+	// 	} else if (isScrollStart) {
+	// 		topNav.style.top = '0';
+	// 		bottomNav.style.bottom = '-4.2rem';
+	// 	} else {
+	// 		topNav.style.top = '0';
+	// 		bottomNav.style.bottom = '0';
+	// 	}
+
+	// 	lastScrollPosition = currentScrollPosition;
+	// };
 </script>
 
 <div class="body">
